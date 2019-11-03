f88c4e86f8856606e6f592db383d1faa14500d90
LeonidLS <llsuschenko@gmail.com>
2019-11-03 15:19:14 +0200

    configuring the listener

--- src/main/java/utils/TestListener.java
+++ src/main/java/utils/TestListener.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,48 +17,52 @@ import java.nio.file.Paths;
 
 public class TestListener implements ITestListener {
 
-  @Override
-  public void onTestStart(ITestResult iTestResult) {
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
 
-  }
-
-  @Override
-  public void onTestSuccess(ITestResult iTestResult) {
-
-  }
-
-  @Override
-  public void onTestFailure(ITestResult iTestResult) {
-    try (InputStream is = Files.newInputStream(Paths.get(captureScreenshot().getPath()))) {
-      Allure.addAttachment("Screenshot", is);
-    } catch (IOException e) {
-      e.printStackTrace();
     }
 
-  }
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+        try (InputStream inputStream = Files.newInputStream(Paths.get(captureScreenshot().getPath()))) {
+            Allure.addAttachment("Screenshot", inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-  @Override
-  public void onTestSkipped(ITestResult iTestResult) {
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+        try (InputStream inputStream = Files.newInputStream(Paths.get(captureScreenshot().getPath()))) {
+            Allure.addAttachment("Screenshot", inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-  }
+    }
 
-  @Override
-  public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
 
-  }
+    }
 
-  @Override
-  public void onStart(ITestContext iTestContext) {
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
 
-  }
+    }
 
-  @Override
-  public void onFinish(ITestContext iTestContext) {
+    @Override
+    public void onStart(ITestContext iTestContext) {
 
-  }
+    }
 
-  @Attachment
-  public File captureScreenshot() {
-    return ((TakesScreenshot) WebDriverFactory.getDriver()).getScreenshotAs(OutputType.FILE);
-  }
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+
+    }
+
+    @Attachment
+    public File captureScreenshot() {
+        return ((TakesScreenshot) WebDriverFactory.getDriver()).getScreenshotAs(OutputType.FILE);
+    }
 }
